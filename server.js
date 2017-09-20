77a2d8809967ed503908d9391b6820ba19903006
Jason McKinney <meter_1@yahoo.com>
2017-09-19 21:33:13 -0500

    worked on server some more!

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var mysql = require("mysql");
+
+var responseArray = [];
 
 // EXPRESS DEFINITION *******************
 var app = express();
@@ -17,9 +20,25 @@ app.use(bodyParser.json({
 	type: "application/vnd.api+json"
 }));
 
+// MYSQL SERVER CONNECTION **************
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "meternx01",
+	database: "databaseHRA"
+});
+
+// DATABASE CONNECTION ********************
+connection.connect(function (err) {
+	if (err) throw err;
+	// if connected correctly
+	console.log("connected as thread id " + connection.threadId + "\n");
+});
+
 // ROUTES	*****************************************************
 
-// ROOT ROUTE-------------------
+// ROOT/INDEX ROUTE-------------------
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -36,24 +55,52 @@ app.get("/reserve", function (req, res) {
 
 // WAITLIST API ROUTE-----------------
 app.get("/api/waitlist", function (req, res) {
-	
+
 	// QUERY FROM DB TO ARRAY OF OBJECTS (WHERE idnum >= 5)
 	return res.json(characters);
 });
 
 // RESERVATIION API ROUTE-----------------
 app.get("/api/tables", function (req, res) {
-	
+
 	// QUERY FROM DB TO ARRAY OF OBJECTS (WHERE 0 < idnum < 5)
-	return res.json(characters);
+	connection.query("SELECT * FROM tableTBL LIMIT 5", function (err, res) {
+		if (err) throw err; // if error - break and show error
+
+		for (i = 0; i < res.length; i++) {
+			responseArray.push(res[i]);
+			console.log("--------------------------");
+		}
+		console.log(responseArray);
+		console.log("--------------------------");
+		return responseArray;
+	});
+	console.log(responseArray);
+	//return res.json(responseArray);
 });
 
-app.post("/api/tables", function(req, res) {
+app.post("/api/tables", function (req, res) {
 	// SEND FORM DATA TO DB
+	var reqTable = req.body;
+	
+	var query = connection.query('INSERT INTO tableTBL(tbl_id,cust_name,cust_phone,cust_email) VALUES (' + reqTable.id + ',"' + 
+					 reqTable.name + '","' + reqTable.phone + '","' + reqTable.email + '")');
+	
+	console.log(query.sql);
+	
+//	connection.query("INSERT INTO tableTBL SET ?", {
+//			cust_name: reqTable.name,
+//			cust_phone: reqTable.phone,
+//			cust_email: reqTable.email,
+//			tbl_id: reqTable.id
+//		},
+//		function (err, res) {
+//			console.log(res.affectedRows + " product inserted!\n");
+//		}
+//	});
 });
 
 // LISTENER
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
 });
-
